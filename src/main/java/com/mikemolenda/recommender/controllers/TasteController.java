4045e7737d0bb5308a8791a3efd1c3cef6f1ec02
Michael P Molenda <molenda.mike@gmail.com>
2017-04-03 12:59:45 -0500

    Database populated

---
+++ src/main/java/com/mikemolenda/recommender/controllers/TasteController.java
@@ -0,0 +1,16 @@
+package com.mikemolenda.recommender.controllers;
+
+import java.io.IOException;
+
+/**
+ * Created by mpm624 on 4/3/17.
+ */
+public class TasteController extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
